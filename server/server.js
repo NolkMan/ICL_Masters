57c485ec8081ad1c4c7b0977627b7d539826fdb4
NolkMan <4n931mail@gmail.com>
2023-06-23 22:06:38 +0100

    basic https server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
-var http = require('http');
+var https = require('https');
+var fs    = require('fs');
+
 var hostname = '127.0.0.1';
-var port = 3000;
+var port = 4333;
+
+const server_options = {
+	key: fs.readFileSync('./ssl/reporting.project-key.pem'),
+	cert: fs.readFileSync('./ssl/reporting.project.pem'),
+}
+
 
 function requestHandler(req, res) {
     res.statusCode = 200;
@@ -8,8 +16,8 @@ function requestHandler(req, res) {
     res.end('Hello World\n');
 }
 
-var server = http.createServer(requestHandler);
+var server = https.createServer(server_options, requestHandler);
 
 server.listen(port, hostname, function () {
-    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
+    console.log("Reporting server running at http://".concat(hostname, ":").concat(String(port), "/"));
 });
